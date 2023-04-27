e0f9fceee8587ebd6271d795a8f3359204a4c315
babes nakatsu <joseph.ilagan.99@gmail.com>
2023-04-27 15:08:34 +0800

    Initial Commit

---
+++ 01-variable-datatypes/01console/script.js
@@ -0,0 +1,21 @@
+// alert('Hello from JS file')
+
+console.log(100);
+
+console.log("Hello World");
+
+
+console.log(20, "Hello", true);
+
+const x = 100
+
+console.log(x);
+
+console.error("Alert");
+console.warn("Warning");
+console.table({name: 'Ryan', email: "ryan@gmail.com"});
+console.group('simple');
+console.log(x);
+console.error("Alert");
+console.warn("warning");
+console.groupEnd();
